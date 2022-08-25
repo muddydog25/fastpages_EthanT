4e23205bed85bbc8d2ef10946d508c566e6e468e
muddydog25 <etran8@gmail.com>
2022-08-24 22:51:27 -0700

    Put the Quiz into quiz.py

--- _action_files/quiz.py
+++ _action_files/quiz.py
@@ -1,3 +1,5 @@
+YesNo = input(print("You are about to take an awesome quiz. Ready? (Yes or No)"))
+
 def q_and_a(prompt):
     print("Question: ")
     student = input()
@@ -7,6 +9,8 @@ def q_and_a(prompt):
     q_and_a("What is 8 x 5?")
     q_and_a("What is 50 / 5?")
 
+
+
 import getpass, sys
 from tkinter.messagebox import YES
 
@@ -15,29 +19,23 @@ def q_and_a(prompt):
     msg = input()
     return msg
 
-questions = 4
-correct = 0
-
-
+questions = 3
+correct = 0 
 
 print('Greetings, ' + getpass.getuser() + " running " + sys.executable)
-print("There will be " + str(questions) + " questions asked.")
-q_and_a("Are you ready to take an awesome short quiz?")
+print("There will be " + str(questions) + " questions asked.")   
 
-rsp = q_and_a("Yes or No?")
-if rsp == "Yes":
-    print("Lets go!")
-
+rsp = q_and_a(YesNo)
+if rsp == "Yes":  
+    print(rsp + "Lets go!")
+    correct += 0
+    
 else:
-    quit()
+    exit()
 
+rsp = q_and_a("How many states are in the United States of America?")
 
-
-# rsp = response 
-
-rsp = q_and_a("test yes or no?")
-
-if rsp == "yes":
+if rsp == "50":
         print(rsp + " correct! ")
         correct += 1
 
@@ -52,6 +50,31 @@ if rsp == "40":
 
 else: 
         print(rsp + " incorrect :(")
+
+rsp = q_and_a("Which was the first dynasty to rule Imperial China?")
+
+if rsp == "Qin Dynasty":
+    
+    print(" correct! ")
+    correct += 1
+
+elif rsp == "The Qin Dynasty":
+    correct += 1
+
+elif rsp == "Qin":
+    correct += 1
+
+else:
+        print(rsp + " incorrect :( ")
+
+if correct == 3:
+    print("Congrats! " + (getpass.getuser() + " You got " + str(correct) +"/" + str(questions)))
+
+elif correct == 2:
+    print("Not bad, keep working! " + (getpass.getuser() + " You got " + str(correct) +"/" + str(questions)))
+
+else:
+    print((getpass.getuser() + " You got " + str(correct) +"/" + str(questions)) + " Its okay, better luck next time! ")
 
 
 
