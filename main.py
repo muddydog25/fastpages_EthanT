546d9df615aa209fa790c845a3d5b3af07623541
realethantran <realethantran@users.noreply.github.com>
2022-09-20 07:03:30 +0000

    deploy: 4f2eebe40ede7e4872c92bf5f37ae53df2f70950

---
+++ main.py
@@ -0,0 +1,18 @@
+
+from __init__ import app  # Definitions initialization
+
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
+@app.route('/')  # connects default URL to index() function
+def index():
+    return render_template("index.html")
+
+# @app.route('/stub/')  # connects /stub/ URL to stub() function
+# def stub():
+#     return render_template("stub.html")
+
+# this runs the application on the development server
+if __name__ == "__main__":
+    app.run(debug=True)
